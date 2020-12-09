0c2352cdc415b11cbe85dadecd382ef0173fc054
Derek-94 <ghdtjrrl94@gmail.com>
2020-12-09 14:44:38 +0900

    feat(main): add saving image function
    - when clicking "save" button, can download what we painted.
    - when context menu was clicked, prevent event from coming up.
    - reorganize the order of functions and eventListeners.

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.height = 700;
-canvas.width = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
-ctx.linkWidth = 2.5;
+canvas.height = CANVAS_SIZE;
+canvas.width = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,canvas.width,canvas.height);
+
+// Initialize drawing line color.
+ctx.strokeStyle = INITIAL_COLOR;
+// Initialize canvas color
+ctx.fillStyle = INITIAL_COLOR
+ctx.lineWidth = 2.5;
 
 let paintingStatus = false;
-
-function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY;
-    if(!paintingStatus){
-        // Not yet we are drawing the pic.
-        ctx.beginPath();
-        ctx.moveTo(x,y) ;
-    }
-    else{
-        ctx.lineTo(x, y);
-        ctx.stroke();
-    }
-}
+let fillingStatus = false;
 
 function startPainting(){
     // When we start painting, we make a event "click" FIRST.
@@ -42,4 +42,83 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", inactivatePaintingStatus);
     canvas.addEventListener("mouseleave", inactivatePaintingStatus);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!paintingStatus){
+        // Not yet we are drawing the pic.
+        ctx.beginPath();
+        ctx.moveTo(x,y) ;
+    }
+    else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function handleCanvasClick(event){
+    if(fillingStatus == true){
+        ctx.fillRect(0,0,canvas.width,canvas.height)
+    };
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+
+// When click the colors below the canvas... 
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+function handleColorClick(event){
+    // setting color of paint.
+    ctx.strokeStyle = event.target.style.backgroundColor;
+
+    // When "fill" button is pressed... ctx.fillStyle will be used.
+    ctx.fillStyle = event.target.style.backgroundColor;
+}
+
+
+// When adjust the range of paint brush...
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+function handleRangeChange(event){
+    ctx.lineWidth = event.target.value;
+}
+
+
+// When click "Paint" or "Fill" button,
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+function handleModeClick(event){
+    if(fillingStatus == false){
+        fillingStatus = true;
+        mode.innerText = "Paint";
+    }
+    else{
+        fillingStatus = false;
+        mode.innerText = "Fill"
+    }
+}
+
+
+// When click "save" button,
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
+
+function handleSaveClick(event){
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS🎨";
+    link.click();
 }
